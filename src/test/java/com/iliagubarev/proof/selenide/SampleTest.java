20f6014ccfe634c23cc576ec35e22c03e450f6e7
ilia.gubarev <ilia.gubarev@t-systems.com>
2017-11-10 19:19:37 +0300

    Sample test improved

--- src/test/java/com/iliagubarev/proof/selenide/SampleTest.java
+++ src/test/java/com/iliagubarev/proof/selenide/SampleTest.java
@@ -1,16 +1,24 @@
 package com.iliagubarev.proof.selenide;
 
+import com.codeborne.selenide.Configuration;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-public class SampleTest1 {
+public class SampleTest {
+
+    @Before
+    public void setBrowser() {
+        Configuration.baseUrl = "http://yandex.ru";
+        Configuration.browser = "chrome";
+    }
 
     @Test
     public void runsSampleTest() {
-        open("http://yandex.ru");
+        open("/images");
         $(By.name("user.name")).setValue("johny");
         $("#submit").click();
         $(".loading_progress").should(disappear); // Waits until element disappears
